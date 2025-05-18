67d93f20293e194bb73ac884fc952ec3c0f2da31
metbuenafe <metbuenafe@tip.edu.ph>
2025-05-18 17:42:11 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,7 @@
 <?php
-session_start(); // Start the session
-
-// Unset all session variables
-$_SESSION = [];
-
-// Destroy the session
+session_start();
+session_unset();
 session_destroy();
-
-// Redirect to the login page
-header("Location: login.html"); // If login.html is your login page
+header("Location: login.html"); // Redirect instantly
 exit();
 ?>
